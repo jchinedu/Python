085f3bc4f36f56351cb574b953feb5aac416fa29
jchinedu <jchinedu080@gmail.com>
2025-05-18 09:57:55 +0100

    'today'

---
+++ class tasks/dice.py
@@ -0,0 +1,35 @@
+"""Simulating the dice game craps"""
+import random
+
+def roll_dice():
+	"""Roll two dice and return their face calues as a turple."""
+	die1 = random.randrange(1,7)
+	die2 = random.randrange(1,7)
+	return (die1, die2)
+def display_dice(dice):
+	"""Display one roll of the two Dice."""
+	die1, die2 = dice
+	print(f"player rolled {die1} + {die2} = {sum(dice)}")
+die_values = roll_dice()
+display_dice(die_values)
+sum_of_dice = sum(die_values)
+if sum_of_dice in (7,11):
+	game_status = 'WON'
+elif sum_of_dice in (2, 3, 12):
+	game_status = 'LOST'
+else:
+	game_status = 'CONTINUE'
+	my_point = sum_of_dice
+	print('point is ', my_point)
+while game_status == 'CONTINUE':
+	die_values = roll_dice()
+	display_dice(die_values)
+	sum_of_dice = sum(die_values)
+	if sum_of_dice == my_point:
+		game_status = 'WON'
+	elif sum_of_dice == 7:
+		game_status = 'LOST'
+if game_status =='WON':
+	print('player wins')
+else:
+	print('player loses')
