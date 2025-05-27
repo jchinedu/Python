b273e9c556f9814882210ce02a51a58a8649ef77
jchinedu <jchinedu080@gmail.com>
2025-05-27 13:34:54 +0100

    'retry'

--- phase gate/todolisttest.py
+++ phase gate/todolisttest.py
@@ -6,9 +6,17 @@ class Testtodolist(TestCase):
      todolist.wait_for_go_back()
     def test_that_add_a_task_function_exists(self):
      todolist.add_a_task()
-    def test_that_add_a_task_function_work_for_number_between_0_to_1(self):
-     self.assertRaises(ValueError,todolist.add_a_task(), 40)
-     self.assertRaises(ValueError,todolist.add_a_task())
+    def test_that_add_a_task_function_returns_correct_answer(self):
+     actual = todolist.add_a_task()
+     expected =None
+     self.assertEqual(actual, expected)
+    def test_that_view_task_function_exists(self):
+     todolist.view_task()
+    #def test_that_mark_a_task_as_complete_function_exists(self):
+    #mark_a_task_as_complete()
+    def test_that_delete_a_task_function_exist(self):
+     Delete_a_task()
+
 
        
            	
