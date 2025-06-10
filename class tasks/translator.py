58bcf8c202c45dcd8f861b19b5de84e5be234890
jchinedu <jchinedu080@gmail.com>
2025-06-10 09:18:04 +0100

    'progress'

--- class tasks/translator.py
+++ class tasks/translator.py
@@ -1,11 +1,12 @@
 # trying to build a translator
 def translate(phrase):
-	translate = " "
+	translate = ""
 	for letter in phrase:
-		if letter in "AEIOUaeiou"
+		if letter in "AEIOUaeiou":
 			translate = translate + "g"
 		else:
 			translate = translate + letter
 	return translate
 
-print(translate(print('enter a phrase')))
+input = print('enter a phrase: ')
+print(translate(input))
