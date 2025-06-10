7fa6cd1c3edfab7d1b11dbed4b29c7a78afe7a38
jchinedu <jchinedu080@gmail.com>
2025-06-10 14:34:28 +0100

    'progress'

--- classes and object/question_app.py
+++ classes and object/question_app.py
@@ -1,7 +1,24 @@
 #trying the multiple choice quiz with objects and classes
-question prompts = [
+from question import Question 
+question_prompts = [
 	"what color are apples?\n (A) Red/Green\n (B) Purple\n (C) Orange\n\n",
 	"what color are banana?\n (A) Teal\n (B) Magenta\n (C) Yellow\n\n",
 	"what color are strawberries?\n (A) Yellow\n (B) Red\n (C) Blue\n\n",
-}
-	
+]
+
+questions = [
+	Question(question_prompts[0], "A"),
+	Question(question_prompts[1], "C"),
+	Question(question_prompts[2], "B")
+
+]
+def run_test(questions):
+	score = 0
+	for question in questions:
+		answer = input(question.prompt)
+		if answer ==question.answer:
+			score += 1
+		else:
+			print('you got the wrong answer, but keep going')
+	print("you got " + str(score) + "/" + str(len(questions)) + "correct")
+run_test(questions)
