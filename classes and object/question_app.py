4acd023007e4f70e7c1a74ed8bb95ed960f3d49f
jchinedu <jchinedu080@gmail.com>
2025-06-10 11:35:09 +0100

    'progress'

---
+++ classes and object/question_app.py
@@ -0,0 +1,7 @@
+#trying the multiple choice quiz with objects and classes
+question prompts = [
+	"what color are apples?\n (A) Red/Green\n (B) Purple\n (C) Orange\n\n",
+	"what color are banana?\n (A) Teal\n (B) Magenta\n (C) Yellow\n\n",
+	"what color are strawberries?\n (A) Yellow\n (B) Red\n (C) Blue\n\n",
+}
+	
