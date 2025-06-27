8ef199c80951ec86135e69c02a0deec062dc114f
jchinedu <jchinedu080@gmail.com>
2025-06-27 12:10:50 +0100

    progress

--- class tasks/list_2d.py
+++ class tasks/list_2d.py
@@ -8,5 +8,24 @@ def main():
             print(f"inner list index: ", index)
 
 
+
+
+#collect the number and return it in words
+def convert_numbers_to_words(numbers):
+    number_to_words = {
+        '1':"one",
+        '2': "two",
+        '3': "three",
+        '4': 'four',
+        '5': 'five',
+        '6': 'six',
+        '7': 'seven',
+        '8': 'eight',
+        '9': 'nine',
+        '10': 'ten',
+    }
+    return number_to_words.get(numbers, "invalid number")
 if __name__ == '__main__':
-    main()
+    numbers = (input("enter a number: "))
+    number_to_words = convert_numbers_to_words(numbers)
+    print(f"the number is {numbers} with {number_to_words} words")
