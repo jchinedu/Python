3382eae819d8b578185d8dbed7c73aa6acfc47bd
jchinedu <jchinedu080@gmail.com>
2025-06-30 15:40:13 +0100

    git

---
+++ class tasks/dictionary_comprehension.py
@@ -0,0 +1,8 @@
+def convert_words_to_numbers(name):
+    result = {(letter, name.count(letter)) for letter in name}
+    return result
+
+
+
+name = input("enter a word: ")
+print(convert_words_to_numbers(name))
