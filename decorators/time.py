5f96de86ab325408417d4605bfc9380ce8d0f146
jchinedu <jchinedu080@gmail.com>
2025-07-18 15:20:12 +0100

    done

--- decorators/time.py
+++ decorators/time.py
@@ -1,19 +1,46 @@
-from tomlkit import value
-
-
 class TimeWithProperties:
-    def __init__(self, second, minute, hour):
+    def __init__(self, second=0, minute=0, hour=0):
         self.second = second
         self.minute = minute
         self.hour = hour
 
     @property
     def second(self):
-        return self.second
+        return self._second
 
-    def seconds(self):
-        if value < 0 and value > 59:
+    @second.setter
+    def seconds(self, value):
+        if  0 > value > 59:
             raise ValueError("seconds must be between 0 and 69")
-        self_second = value
+        self._second = value
 
-    time1 = timeWithProperties()
+    @property
+    def minute(self):
+        return self._minute
+
+
+    @minute.setter
+    def minute(self, value):
+        if 0 > value > 59:
+            raise ValueError("minutes must be between 0 and 59")
+        self._minute = value
+
+    @property
+    def hour(self):
+        return self._hour
+
+    @hour.setter
+    def hour(self, value):
+        if 0 > value > 23:
+            raise ValueError("hours must be between 0 and 23")
+        self._hour = value
+
+    @second.setter
+    def second(self, value):
+        self._second = value
+
+    def __str__(self):
+        return f"{self.second} {self.minute} {self.hour}"
+
+
+time1 = TimeWithProperties()
