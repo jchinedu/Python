f33eaa3338e3a3481649cbb3c25765b4c297b4ee
jchinedu <jchinedu080@gmail.com>
2025-07-24 22:50:19 +0100

    done

--- chapter_2/Even_Number.py
+++ chapter_2/Even_Number.py
@@ -2,5 +2,7 @@ print('number checker')
 number = int(input('Enter any number: '))
 if number % 2 == 0:
 	print("this number is an even number")
+else:
+	print("this number is not an even number")
 
  
