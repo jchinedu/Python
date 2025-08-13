67b89eaeb0d766bc152e149126f9c722d8cba1fb
jchinedu <jchinedu080@gmail.com>
2025-08-13 17:43:56 +0100

    'progress'

--- class tasks/reverse_words.py
+++ class tasks/reverse_words.py
@@ -1,14 +1,3 @@
 def reverse_words(text):
     result = ""
-    word = ""
-
-    for char in text:
-        if char != ' ':
-            word += char
-        else:
-            result += word[::-1]  
-            result += ' '         
-            word = ''             
-
-    result += word[::-1]  
-    return result
+    
